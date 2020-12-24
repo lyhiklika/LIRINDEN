a31c42c008bebadf6b9330a92d4c7f824a575e54
irkibaln <61134265+irkibaln@users.noreply.github.com>
2020-12-24 23:52:25 +0300

    Update file_system_control.py

--- file_system_control.py
+++ file_system_control.py
@@ -38,4 +38,44 @@ def stop():
 def exxit():
     top.destroy()
 
+top=tkinter.Tk()
+top.title('File_Control')
+top.geometry('1100x370')
 
+startButton = tkinter.Button(top, height = 2, width = 20,text ="Начать запись.", command = start)
+startButton.grid(row = 1, column = 0)
+
+stopButton = tkinter.Button(top, height = 2, width = 20,text ="Остановить запись.", command = stop)
+stopButton.grid(row = 2, column = 0)
+
+stopButton = tkinter.Button(top, height = 2, width = 20,text ="Просмотр логов.", command = spisok)
+stopButton.grid(row = 3, column = 0)
+
+exitButton = tkinter.Button(top, height = 2, width = 20,text ="Выход?", command = exxit)
+exitButton.grid(row = 4, column = 0)
+
+text = tkinter.Text(width=100, height = 20)
+text.grid(row = 0, column = 6, rowspan = 10, columnspan = 5)
+
+l1 = tkinter.Label(top, text = 'Логи не записываются :(')
+l1.grid(row=0, column = 0)
+l1.config(bd = 20, bg = '#ff0000')
+
+l2 = tkinter.Label(top, text = 'Выберите время начала и конца логов:')
+l2.grid(row=0, column = 1,columnspan = 5)
+
+ld = tkinter.Label(top, text = 'День:')
+ld.grid(row=1, column = 2)
+lm = tkinter.Label(top, text = 'Месяц:')
+lm.grid(row=1, column = 3)
+ly = tkinter.Label(top, text = 'Год:')
+ly.grid(row=1, column = 4)
+ly = tkinter.Label(top, text = 'Время(hh:mm):')
+ly.grid(row=1, column = 5)
+
+lm = tkinter.Label(top, text = 'Начало:')
+lm.grid(row=2, column = 1)
+ly = tkinter.Label(top, text = 'Конец:')
+ly.grid(row=3, column = 1)
+ln = tkinter.Label(top, text = 'Название файла:')
+ln.grid(row=4,column=1,columnspan=2)
